ec07e2ca95bed40149bebbe39f0ca12eb9cbe315
NorbertSan <norbertsanpruch@gmail.com>
2020-03-26 23:18:16 +0100

    create files structure

---
+++ src/GlobalStyles/index.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import theme from "theme";
+
+const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
+*,*::after,*::before{
+    box-sizing:border-box;
+}
+html{
+    font-size:62.5%;
+}
+   body{
+        font-size:${theme.fontSize.s};
+        margin:0;
+        font-family: 'Roboto', sans-serif;
+        background:${theme.whiteColor};
+        color:${theme.primaryColor};
+    }  
+`;
+
+export default GlobalStyles;
---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+const theme = {
+  primaryColor: "#204051",
+  secondaryColor: "#D8EBB5",
+  tertiaryColor: "#639A67",
+  whiteColor: "#fff",
+  fontSize: {
+    xs: "1.4rem",
+    s: "1.6rem",
+    m: "2.2rem",
+    l: "2.8rem",
+    xl: "4.2rem"
+  },
+  fontWeight: {
+    regular: "400",
+    medium: "500",
+    bold: "700"
+  }
+};
+
+export default theme;
